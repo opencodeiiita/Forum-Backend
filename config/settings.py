297820b7c6a2b15ee67e28528ced5a89ad9bbff6
Vaibhav Bajaj <bajajv2003@gmail.com>
2022-12-18 00:20:22 +0530

    Added forums and accounts to settings.py By contributor VBajaj113

--- config/settings.py
+++ config/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'forum.apps.ForumConfig',
+    'accounts.apps.AccountsConfig',
 ]
 
 MIDDLEWARE = [
