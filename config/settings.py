192608f6432ae40bb35a78b0784ce5a575a39c52
SyedMa3 <sammateen10@outlook.com>
2022-12-19 00:26:11 +0530

    fixed merge conflict

--- config/settings.py
+++ config/settings.py
@@ -38,7 +38,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'forum.apps.ForumConfig',
     'accounts.apps.AccountsConfig',
     'forum',
 ]
