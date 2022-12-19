f298860c8fb66ffa87f0d51de112279bdd6473cd
SyedMa3 <sammateen10@outlook.com>
2022-12-19 12:52:35 +0530

    added user to answer

--- forum/models.py
+++ forum/models.py
@@ -1,15 +1,15 @@
 from django.db import models
-import uuid
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Answer(models.Model):
     created_at = models.DateField(auto_now_add=True)
     updated_at = models.DateField(auto_now=True) #updated_date can also be used to check if edited
-    content = models.CharField(max_length=200)
+    content = models.TextField()
     # question_related = models.ForeignKey(Question, on_delete=models.CASCADE)
     upvotes = models.IntegerField(default=0)
     downvotes = models.IntegerField(default=0)
-
+    user = models.ForeignKey(User, on_delete=models.CASCADE, default='NO USER')
 
     def __str__(self):  
         return str(self.id)
