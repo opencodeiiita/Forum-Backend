6f9f6e1764b08638855d5005cbe1dbd25a9e01d8
SyedMa3 <sammateen10@outlook.com>
2022-12-20 19:55:11 +0530

    refactored question model

--- forum/models.py
+++ forum/models.py
@@ -13,3 +13,21 @@ class Answer(models.Model):
 
     def __str__(self):  
         return str(self.id)
+
+class Question(models.Model):
+    # Fields for the question text and the date/time it was asked
+    title = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published', auto_now_add=True)
+    updated_date = models.DateTimeField('date updated', auto_now=True)
+
+    # Additional fields for storing additional information about the question
+    # such as the user who asked it and any tags or categories it belongs to
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    # tags = models.ManyToManyField(Tag)
+    # category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
+    description = models.TextField()
+    upvotes = models.PositiveIntegerField(default=0)
+    answers = models.PositiveIntegerField(default=0)
+
+    def __str__(self):
+        return f'{self.id}: {self.title}'
