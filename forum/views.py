5ef18051b9364d2e8a5fb857e6dbef44a8fa98e6
SyedMa3 <sammateen10@outlook.com>
2022-12-20 19:55:26 +0530

    added listanswer view

--- forum/views.py
+++ forum/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.views.generic.list import ListView
+
+from forum.models import Answer, Question
 
 # Create your views here.
+
+# class ListAnswer(ListView):
+
+#     model = Answer
+
+#     def get_queryset(self):
+#         self.question = get_object_or_404(Question, name=self.kwargs['question'])
+#         qs = super().get_queryset().filter(question_related=self.question).order_by("upvotes")
+#         return qs
