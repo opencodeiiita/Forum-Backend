e6959c02639e3c54de19b7ca94040c0c7f1b3bd1
mugdha273 <mugdhasharma0327@gmail.com>
2022-12-21 10:00:37 +0530

    data adds author username rather than pk

--- forum/serialzers.py
+++ forum/serialzers.py
@@ -2,6 +2,9 @@ from rest_framework import serializers
 from forum.models import Question
 
 class QuestionSerialzers(serializers.ModelSerializer):
+    #overriding author field to return author's name
+    author = serializers.CharField(source='author.username')
+    
     class Meta:
         model = Question
-        fiels = '__all__'
+        fields = '__all__'
