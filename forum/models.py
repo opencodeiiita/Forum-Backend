be0b03b8b6b9266812bc7f5f53ac74ef256d9cd7
MistyRavager <kushagra.gupta0103@gmail.com>
2022-12-24 20:46:55 +0530

    updated fix #37 : added unique_together field, commented out UniqueConstraint

--- forum/models.py
+++ forum/models.py
@@ -29,7 +29,7 @@ class Question(models.Model):
     upvotes = models.PositiveIntegerField(default=0)
     answers = models.PositiveIntegerField(default=0)
     class Meta:
-        models.UniqueConstraint(fields=['author','answers'],name='unique pair of author and answers')
-
+      #  models.UniqueConstraint(fields=['author','answers'],name='unique pair of author and answers')
+        unique_together = [['author','answers']]
     def __str__(self):
         return f'{self.id}: {self.title}'
