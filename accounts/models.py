68048e3122d1544170059d31a216cf00bcba5583
mugdha273 <mugdhasharma0327@gmail.com>
2022-12-25 14:33:46 +0530

    Implement a Token setup for post save as a reciever for User

--- accounts/models.py
+++ accounts/models.py
@@ -1,11 +1,22 @@
-from django.db import models
-from ckeditor.fields import RichTextField
-from django.contrib.auth.models import User
 import os
 from io import BytesIO
-from PIL import Image 
+
+from ckeditor.fields import RichTextField
+from django.conf import settings
+from django.contrib.auth.models import User
 from django.core.files.base import ContentFile
 from django.core.files.uploadedfile import InMemoryUploadedFile
+from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from PIL import Image
+from rest_framework.authtoken.models import Token
+
+
+@receiver(post_save, sender=settings.AUTH_USER_MODEL)
+def gen_auth_token(sender, instance=None, created=False, **kwargs):
+    if created:
+        Token.objects.create(user=instance)
 
 # Create your models here.
 class Profile(models.Model):
