9024fc62328aff6d79e8427da5374a60f9b5460b
MistyRavager <kushagra.gupta0103@gmail.com>
2022-12-28 12:43:37 +0530

    fix for issue #60: created logout view

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from django.contrib.auth import logout
 # Create your views here.
+
+
+def logout_view(request):
+    if request.method == "POST":
+        logout(request)
+        return redirect("/") #give path to after logout page
+    return render(request,"",{}) # give path to what page to render in 2nd argument
--- forum/urls.py
+++ forum/urls.py
@@ -1,10 +1,11 @@
 from django.urls import include, path
 from rest_framework import routers
 from forum import views
-
+from accounts.views import logout_view
 router = routers.DefaultRouter()
 router.register(r'answers', views.AnswerView)
 router.register(r'questions', views.QuestionView)   
+router.register(r'logout',logout_view)
 
 urlpatterns = [
     path('', include(router.urls)),
